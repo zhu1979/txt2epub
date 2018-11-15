99650fc1d71a08e858d2d0198a58b5c64c9ca876
zhu1979 <ahming98@hotmail.com>
2018-11-15 17:01:13 +0800

    2018-11-15，按照EPUB3的目录结构修改

--- txt2epub.py
+++ txt2epub.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # coding:utf-8
 
-#
+# Creater: Andy Lau
+# Modify by zhu1979
 # Convert Chinese novel text file into an epub file.
 # Assume each chapter has been converted to html,
 
@@ -17,93 +18,180 @@ import codecs
 from lxml import etree
 from docopt import docopt
 
-# uncomment to debug
-#import logging
-#import pudb; pu.db
 
 inputcode = ''
 
-usage_info = """Usage: txt2epub.py  --output <outputfolder>  --name <name> | --help
-
-
+usage_info = """
+Usage: txt2epub.py  --output <outputfolder>  --name <name>  --author <author>| --help
 Arguments:
-    --output  folder where EPUB will be created or updated
-    --name    name of content that will be displayed in epub
-    --help    display this message
+    --output    folder where EPUB will be created or updated
+    --name      name of content that will be displayed in epub
+    --author    author of the book
+    --help      display this message """
 
-"""
 
 def make_new_epub_folder(options):
-    '''given docopt's arguments dict: create an empty epub folder if it does not exist'''
-    epubdir = os.path.abspath(os.path.join(os.curdir, options['<outputfolder>']))
-
-    name = options['<name>'].replace(' ', '-')
-    EPUBdir = os.path.join(epubdir, 'EPUB')
-    METAdir = os.path.join(epubdir, 'META-INF')
+    ''' given docopt's arguments dict:
+    create an empty epub folder if it does not exist '''
+    epubdir = os.path.abspath(os.path.join(
+        os.curdir, options['<outputfolder>']))
 
     if not os.path.isdir(epubdir):
         os.makedirs(epubdir)
+
+    # name = options['<name>'].replace(' ', '-')
+    EPUBdir = os.path.join(epubdir, 'OEBPS')
+    METAdir = os.path.join(epubdir, 'META-INF')
+
+    Textdir = os.path.join(EPUBdir, 'Text')
+    Stylesdir = os.path.join(EPUBdir, 'Styles')
+
     if not os.path.isdir(EPUBdir):
         os.mkdir(EPUBdir)
     if not os.path.isdir(METAdir):
         os.mkdir(METAdir)
-
+    if not os.path.isdir(Textdir):
+        os.mkdir(Textdir)
+    if not os.path.isdir(Stylesdir):
+        os.mkdir(Stylesdir)
     return True
 
-def zh2unicode(stri):
-        """Auto converter encodings to unicode
-
-        It will test utf8,gbk,big5,jp,kr to converter"""
-        for c in ('utf-8', 'gbk', 'big5', 'jp','euc_kr','utf16','utf32'):
-            try:
-                return stri.decode(c)
-            except:
-                pass                
-        return stri
-
-def zh2utf8(stri):
-        """Auto converter encodings to utf8
-
-        It will test utf8,gbk,big5,jp,kr to converter"""
-        for c in ('utf-8', 'gbk', 'big5', 'jp',
-'euc_kr','utf16','utf32'):
-                try:
-                        return stri.decode(c).encode('utf8')
-                except:
-                        pass
-        return stri
 
 def is_chapter_title(line):
-    #if re.match(ur"[正文]*\s*[第终][0123456789一二三四五六七八九十百千万零 　\s]*[章部集节卷]", unicode(line,'utf-8')) :
-    if re.match(ur"[正文]*\s*[第终][0123456789一二三四五六七八九十百千万零 　\s]*[章部集节卷]", zh2unicode(line)) :
+    # if re.match(ur"[正文]*\s*[第终][0123456789一二三四五六七八九十百千万零 　\s]*[章部集节卷]", unicode(line,'utf-8')) :
+    reg1 = re.compile(r"\s*[第终][0123456789一二三四五六七八九十百千万零 　\s]*[章部集节卷]")
+    reg2 = re.compile(r'^[0-9]{1,4} .*')
+    reg3 = re.compile(r'^\s*[第终卷][0123456789一二三四五六七八九十零〇百千两]*[章回部节集卷].*')
+    if reg1.match(line):
         return True
-    else: 
+    elif reg2.match(line):
+        return True
+    elif reg3.match(line):
+        return True
+    else:
         return False
 
-def makechapterhtml(filepath, chapter, chapnum):
-    filepath = os.path.join(filepath, 'EPUB') 
-    with open(os.path.join(filepath, '{:0>4d}.xhtml'.format(chapnum)), 'w') as f:
-        f.write(r'<?xml version="1.0" encoding="UTF-8"?>')
-        f.write("""<html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops">
-  <head>
-  <title>"""+bookname+"""</title>
-  <link rel="stylesheet" href="stylesheet.css" type="text/css" />
-  </head>
-  <body>
-""")
-        f.write('<h2>' + chapter[0] + '</h2>\n')
-        f.write(chapter[1] + '\n')
-        f.write("""</body>
-</html>""")
+
+def write_style_css(filepath):
+    filepath = os.path.join(filepath, r'OEBPS\Styles')
+    css = '''/*定义字体名称*/
+    @font-face {
+        font-family: "h1";
+        src: local("FZSongKeBenXiuKaiS-R-GB");
+    }
+
+    @font-face {
+        font-family: "h2";
+        src: local("FZQingKeBenYueSongS-R-GB");
+    }
+
+    @font-face {
+        font-family: "h3";
+        src: local("FZQingKeBenYueSongS-R-GB");
+    }
+
+    @font-face {
+        font-family: "zw";
+        src: local("FZBoYaFangKanSongK");
+    }
+
+    @font-face {
+        font-family: "st";
+        src: local("FZNewShuSong-Z10S");
+    }
+
+    body {
+        padding: 0%;
+        margin-top: 0px;
+        margin-bottom: 0px;
+        margin-left: 0px;
+        margin-right: 0px;
+        line-height: 110%;
+        orphans: 0;
+        widows: 0;
+    }
+
+    p {
+        font-family: "zw";
+        font-size: 125%;
+        line-height: 110%;
+        text-indent: 0em;
+        margin-top: 4px;
+        margin-bottom: 0px;
+        margin-left: 0px;
+        margin-right: 0px;
+        orphans: 0;
+        widows: 0;
+    }
+
+    div {
+        line-height: 110%;
+    }
+
+    .booktitle {
+        font-size: 200%;
+        font-family: "h1";
+        text-align: right;
+        margin-top: 200px;
+        margin-right: 40px;
+    }
+
+    .bookauthor {
+        font-size: 100%;
+        font-family: "h3";
+        text-align: right;
+        margin-right: 40px;
+    }
+
+    h2,
+    .titlel2std {
+        color: pink;
+        margin-top: 0;
+        line-height: 100%;
+        text-align: right;
+        border-style: none solid none none;
+        /*上，右，下，左*/
+        border-width: 0px 20px 0px 0px;
+        border-right-color: mediumvioletred;
+        background-color: palevioletred;
+        padding: 20px 5px 5px 5px;
+        page-break-before: always;
+        font-weight: bold;
+        font-size: x-large;
+        font-family: "h2", "zw", serif;
+    }'''
+    with open(os.path.join(filepath, 'stylesheet.css'), 'w', encoding='utf-8') as f:
+        f.write(css)
         f.close()
 
+
+def makechapterhtml(filepath, chapter, chapnum):
+    filepath = os.path.join(filepath, r'OEBPS\Text')
+    with open(os.path.join(filepath, '{:0>4d}.xhtml'.format(chapnum)), 'w', encoding='utf-8') as f:
+        f.write(r'<?xml version="1.0" encoding="UTF-8"?>')
+        f.write("""
+        <html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops">
+        <head>
+                <title>"""+bookname+"""</title>
+                <link rel="stylesheet" href="../Styles/stylesheet.css" type="text/css" />
+        </head>
+        <body>""")
+        f.write('        <h2>' + chapter[0] + '</h2>\n')
+        f.write(chapter[1] + '\n')
+        f.write("""
+        </body>
+        </html>""")
+        f.close()
+
+
 def writeopffile(filepath, manifest, spine):
-    index_tpl = '''<package version="3.0" xmlns="http://www.idpf.org/2007/opf" unique-identifier="uid">
+    index_tpl = '''<?xml version="1.0" encoding="utf-8" standalone="no"?>
+<package version="3.0" xmlns="http://www.idpf.org/2007/opf" unique-identifier="uid">
     <metadata xmlns:dc="http://purl.org/dc/elements/1.1/">
-        <dc:identifier id="uid">Andy Lau txt2epub.1.0</dc:identifier>
+        <dc:identifier id="uid">txt2epub.1.1</dc:identifier>
         <dc:title>%(bname)s</dc:title>
-        <dc:creator>Andy Lau</dc:creator>
-        <dc:language>en</dc:language>
+        <dc:creator>%(artname)s</dc:creator>
+        <dc:language>zh-CN</dc:language>
         <meta property="dcterms:modified">%(moditime)s</meta>
     </metadata>
     <manifest>
@@ -112,68 +200,72 @@ def writeopffile(filepath, manifest, spine):
     <spine toc="ncx">
         %(spine)s
     </spine>
+    <guide>
+        <reference href="OEBPS/0000.xhtml" type="text" title="Beginning"/>
+    </guide>
 </package>'''
 
-    currenttime = time.strftime('%Y-%m-%dT%H:%M:%SZ',time.localtime())
+    currenttime = time.strftime('%Y-%m-%dT%H:%M:%SZ', time.localtime())
 
-    with open(os.path.join(filepath, 'content.opf'), 'w') as f:
+    with open(os.path.join(filepath, r'OEBPS\content.opf'), 'w', encoding='utf-8') as f:
         f.write(index_tpl % {
             'bname': bookname,
+            'artname': author,
             'moditime': currenttime,
             'manifest': manifest,
             'spine': spine,
         })
 
-#EPUB2 NCX file, abandoned in EPUB3
+# EPUB2 NCX file, abandoned in EPUB3
+
+
 def writencxfile(filepath, navpoint):
     index_ncx = '''<?xml version="1.0" encoding="UTF-8"?>
 <ncx xmlns="http://www.daisy.org/z3986/2005/ncx/" version="2005-1">
-  <head>
-     <meta name="dtb:uid" content="Andy Lau txt2epub.1.0"/>
-     <meta name="dtb:depth" content="1"/>
-     <meta name="dtb:totalPageCount" content="0"/>
-     <meta name="dtb:maxPageNumber" content="0"/>
-  </head>
-  <docTitle>
+<head>
+    <meta name="dtb:uid" content="txt2epub.1.1"/>
+    <meta name="dtb:depth" content="1"/>
+    <meta name="dtb:totalPageCount" content="0"/>
+    <meta name="dtb:maxPageNumber" content="0"/>
+</head>
+<docTitle>
     <text>%(bname)s</text>
-  </docTitle>
-  <navMap>
+</docTitle>
+<docAuthor>
+    <text>%(artname)s</text>
+</docAuthor>
+<navMap>
     %(navpoint)s
-  </navMap>
+</navMap>
 </ncx>'''
 
-    with open(os.path.join(filepath, 'toc.ncx'), 'w') as f:
+    with open(os.path.join(filepath, r'OEBPS\toc.ncx'), 'w', encoding='utf-8') as f:
         f.write(index_ncx % {
             'bname': bookname,
+            'artname': author,
             'navpoint': navpoint,
         })
 
-#EPUB3 END file
+# EPUB3 END file
+
+
 def writenavfile(filepath, olli):
-    index_nav = '''<html xmlns="http://www.w3.org/1999/xhtml" 
-      xmlns:epub="http://www.idpf.org/2007/ops">
-  <head>
-    <title>%(bname)s</title>
-  </head>
-  <body>
-    <section epub:type="frontmatter toc">
-      <header>
-        <h1>目  录</h1>
-      </header>
-      <nav epub:type="toc" id="toc">
-      <ol>
+    index_nav = '''<?xml version="1.0" encoding="UTF-8"?>
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops">
+<head>
+    <title>Table of Contents</title>
+</head>
+<body epub:type="frontmatter">
+    <nav epub:type="toc" id="toc"><h1>Table of Contents</h1>
+    <ol>
         %(olli)s
-      </ol>
-      </nav>
-    </section>
-  </body>
+    </ol>
+    </nav>
+</body>
 </html>'''
 
-    with open(os.path.join(filepath, 'nav.xhtml'), 'w') as f:
-        f.write(index_nav % {
-            'bname': bookname,
-            'olli': olli,
-        })
+    with open(os.path.join(filepath, r'OEBPS\Text\nav.xhtml'), 'w', encoding='utf-8') as f:
+        f.write(index_nav % {'olli': olli})
 
 
 def make_container(docoptions):
@@ -183,28 +275,22 @@ def make_container(docoptions):
     if not os.path.exists(metafolder):
         os.makedirs(metafolder)
     containerpath = os.path.join(metafolder, "container.xml")
-    if os.path.exists(containerpath):
-        try:
-            container = etree.parse(containerpath)
-        except etree.XMLSyntaxError:
-            container = etree.XML('<container xmlns="urn:oasis:names:tc:opendocument:xmlns:container" version="1.0""><rootfiles>\n</rootfiles></container>')
-    else:
-        container = etree.XML('<container xmlns="urn:oasis:names:tc:opendocument:xmlns:container" version="1.0"><rootfiles>\n</rootfiles></container>')
-
-    packagefilepath = "content.opf"
-    current_rootfiles = [r.attrib['full-path'] for r in container.findall('.//{urn:oasis:names:tc:opendocument:xmlns:container}rootfile')]
-    if packagefilepath not in current_rootfiles:
-        rootfile = etree.Element('rootfile', attrib={'media-type':"application/oebps-package+xml", 'full-path':packagefilepath})
-        rootfile.tail = '\n'
-        container.find('.//{urn:oasis:names:tc:opendocument:xmlns:container}rootfiles').append(rootfile)
-
-    return container
+    container = '''<?xml version="1.0"?>
+<container xmlns="urn:oasis:names:tc:opendocument:xmlns:container" version="1.0">
+    <rootfiles>
+        <rootfile full-path="OEBPS/content.opf" media-type="application/oebps-package+xml"/>
+    </rootfiles>
+</container>'''
+    with open(containerpath, 'w', encoding='utf-8') as f:
+        f.write(container)
+        f.close()
 
 
 if __name__ == "__main__":
     arguments = docopt(usage_info, sys.argv[1:])
     outputfolder = arguments['<outputfolder>']
     bookname = arguments['<name>']
+    author = arguments['<author>']
     make_new_epub_folder(arguments)
 
     chapters = []
@@ -213,95 +299,94 @@ if __name__ == "__main__":
     pre_chap_title = ''
     frontpage = 0
 
-    bookfile = open('{bname}.txt'.format(bname = bookname),'r')
-    for linenum,line in enumerate(bookfile.readlines()):
+    bookfile = open('{bname}.txt'.format(bname=bookname), 'r',encoding='utf-8')
+    for linenum, line in enumerate(bookfile.readlines()):
         line = line.strip()
-        if  len(line):
-            line = zh2utf8(line)
+        if len(line):
             if is_chapter_title(line):
-                    pre_chap_title = chaptername
-                    chaptername = line
-                    if linenum <> 0 and not frontpage :
-                        frontpage = 1
-                        pre_chap_title = '前  言'
+                pre_chap_title = chaptername
+                chaptername = line
+                if linenum != 0 and not frontpage:
+                    frontpage = 1
+                    pre_chap_title = '前  言'
 
-                    if frontpage :
-                        chapters.append((pre_chap_title, chaptercontent))
-                    else:                    
-                        chapters.append((chaptername, chaptercontent))
-                    chaptercontent = ''
+                if frontpage:
+                    chapters.append((pre_chap_title, chaptercontent))
+                else:
+                    chapters.append((chaptername, chaptercontent))
+                chaptercontent = ''
             else:
-                chaptercontent += '<p>'+line+'</p>\n'
+                chaptercontent += '        <p>'+'　　'+line+'</p>\n'
 
     chapters.append((chaptername, chaptercontent))
 
 #        EPUB2, abandon in EPUB3
     manifest = '<item id="ncx" href="toc.ncx" media-type="application/x-dtbncx+xml"/>\n'
-    manifest += '<item id="toc" properties="nav" href="nav.xhtml" media-type="application/xhtml+xml"/>\n'
+    manifest += '        <item id="toc" properties="nav" href="Text/nav.xhtml" media-type="application/xhtml+xml"/>\n'
+    manifest += '        <item id="css" href="Styles/stylesheet.css" media-type="text/css"/>\n'
     spine = ''
     olli = ''
 
 #        EPUB2, abandon in EPUB3
     navpoint = ''
+    # chapter_file_list = []
 
     for chapternum, chapter in enumerate(chapters):
         makechapterhtml(outputfolder, chapter, chapternum)
-        manifest += '<item id="chapter_{:0>4d}" href="EPUB/{:0>4d}.xhtml" media-type="application/xhtml+xml"/>\n'.format(chapternum, chapternum)
-        spine += '<itemref idref="chapter_{:0>4d}" />\n'.format(chapternum)
+        manifest += '        <item id="chapter_{:0>4d}" href="Text/{:0>4d}.xhtml" media-type="application/xhtml+xml"/>\n'.format(
+            chapternum, chapternum)
+        spine += '        <itemref idref="chapter_{:0>4d}" />\n'.format(chapternum)
 
-        olli += '''        <li id="chapter_{:0>4d}">
-          <a href="EPUB/{:0>4d}.xhtml">{chaptitle}</a>
+        olli += '''<li id="chapter_{:0>4d}">
+          <a href="../Text/{:0>4d}.xhtml">{chaptitle}</a>
         </li>
-          '''.format(chapternum, chapternum, chaptitle = chapter[0])
+          '''.format(chapternum, chapternum, chaptitle=chapter[0])
 
 #        EPUB2, abandon in EPUB3
-        navpoint += '''  <navPoint class="chapter" id="chapter_{seq}" playOrder="{chapnum}">
-    <navLabel>
-      <text>{chaptitle}</text>
-    </navLabel>
-    <content src="EPUB/{seq}.xhtml"/>
-  </navPoint>
-'''.format(seq = '{:0>4d}'.format(chapternum), chaptitle = chapter[0], chapnum = chapternum)
+        navpoint += '''    <navPoint class="chapter" id="chapter_{seq}" playOrder="{chapnum}">
+        <navLabel>
+        <text>{chaptitle}</text>
+        </navLabel>
+        <content src="Text/{seq}.xhtml"/>
+    </navPoint> '''.format(seq='{:0>4d}'.format(chapternum), chaptitle=chapter[0], chapnum=chapternum)
+        chapter_file_name = '{:0>4d}.xhtml'.format(chapternum)
+        # chapter_file_list.append(chapter_file_name)
 
-    #write package.opf
+    # write OEBPS/stylesheet.css
+    write_style_css(outputfolder)
+
+    # write package.opf
     writeopffile(outputfolder, manifest, spine)
 
 #   EPUB2, abandon in EPUB3
-    #write toc.ncx
+    # write toc.ncx
     writencxfile(outputfolder, navpoint)
 
-    #write nav.xhtml
+    # write nav.xhtml
     writenavfile(outputfolder, olli)
 
-    #write container.xml
+    # write container.xml
     epubfolder = arguments['<outputfolder>']
-    metafolder = os.path.join(epubfolder, 'META-INF')
-    containerpath = os.path.join(metafolder, "container.xml")
-    container = make_container(arguments)
-    with open(containerpath, 'w') as f:
-        f.write(etree.tostring(container, pretty_print=True))
+
+    make_container(arguments)
 
     # write mimetype
-    with open(os.path.join(epubfolder, 'mimetype'), 'w') as f:
+    with open(os.path.join(epubfolder, 'mimetype'), 'w', encoding='utf-8') as f:
         f.write('application/epub+zip')
         f.close()
 
-    ## finally zip everything into the destination
-    out = zipfile.ZipFile(os.path.join(epubfolder, '{bname}.epub'.format(bname = bookname)), "w", zipfile.ZIP_DEFLATED)
+    # finally zip everything into the destination
+    out = zipfile.ZipFile(os.path.join(epubfolder, '{bname}.epub'.format(
+        bname=bookname)), "w", zipfile.ZIP_DEFLATED)
+
     out.write(epubfolder + "/mimetype", "mimetype", zipfile.ZIP_STORED)
-    
-    for root, dirs, files in os.walk(epubfolder):  
-        for name in files:  
-            if name <> 'mimetype' :
-               fname = os.path.join(root, name)
-               new_path = os.path.normpath(fname.replace(epubfolder,''))
-               out.write(fname, new_path)
-    #for p in os.listdir(epubfolder):
-    #    if os.path.isdir(p):
-    #        for f in os.listdir(p):
-    #            logging.warning("Writing file '%s/%s'" % (p, f))
-    #            out.write(os.path.join(p, f), zipfile.ZIP_DEFLATED)
-    #    else:
-    #        out.write(os.path.join(epubfolder, p))
+    out.write(epubfolder + "/META-INF/container.xml", "META-INF/container.xml", zipfile.ZIP_DEFLATED)
+
+    for root, dirs, files in os.walk(epubfolder):
+        for name in files:
+            if name != 'mimetype' and name != 'container.xml':
+                fname = os.path.join(root, name)
+                new_path = os.path.normpath(fname.replace(epubfolder, ''))
+                out.write(fname, new_path)
 
     out.close()
